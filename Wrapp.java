11bdbd87a681631d9079f2d802ce988d675792bb
anjankumardas5 <anjantechnology507@gmail.com>
2024-06-26 01:58:10 +0530

    Command line Argument

--- Wrapp.java
+++ Wrapp.java
@@ -3,8 +3,13 @@ import java.util.*;
 class Wrapp{
 public static void main(String args[]){
 int y=Integer.parseInt("123");
+
 Integer x=Integer.valueOf("1001",2);
 int a=x.intValue();
+
+Double A=Double.valueOf("56");
+double ans=A.doubleValue();
+System.out.println(ans);
 System.out.println(a);
 System.out.println(y);
 }
