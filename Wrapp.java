d9efa265eb6764c57eb6094a9b97a4596a4c7b9a
anjankumardas5 <anjantechnology507@gmail.com>
2024-06-25 23:41:37 +0530

    wrapper class

---
+++ Wrapp.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Wrapp{
+public static void main(String args[]){
+int y=Integer.parseInt("123");
+Integer x=Integer.valueOf("1001",2);
+int a=x.intValue();
+System.out.println(a);
+System.out.println(y);
+}
+}
+
