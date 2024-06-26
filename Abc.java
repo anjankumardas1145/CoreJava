108fa139358de449b3cad0d7a4eaac80e88e67ad
anjankumardas5 <anjantechnology507@gmail.com>
2024-06-27 00:15:07 +0530

    command line argument

---
+++ Abc.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Abc{
+public static void main(String Anjan[]){
+int sum=0,avg=0;
+for(int i=0;i<Anjan.length;i++){
+sum=sum+Integer.parseInt(Anjan[i]);
+
+}
+ avg=sum/Anjan.length;
+System.out.println("the average of the number is:"+ avg);
+}
+}
