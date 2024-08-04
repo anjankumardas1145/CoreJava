c7dfd5d02e2bc4c33240ccde27c44475ecea3237
anjankumardas5 <anjantechnology507@gmail.com>
2024-08-04 22:48:34 +0530

    a class implement multiple interface in java

--- InterFace.java
+++ InterFace.java
@@ -1,10 +1,20 @@
 	interface A{
-void show();//by default in interface every method is "abstract public"
+void show();  //by default in interface every method is "abstract public"
 void drive();
-//int age;error bcz variable in bydefault final and static so we need to declare the value
+                          //int age;error bcz variable in bydefault final and static so we need to declare the value
 int age=22;
 }
-class B implements A{
+interface C{
+/*void run(){
+System.out.println("Running function of interface C");
+}*/
+      void display();
+
+
+int age=21;
+String name="Anjan";
+}
+class B implements A,C{//A class can implement multiple interface
 //here we are mendatory to use the method of A
 public void show(){
 System.out.println("Shoe function");
@@ -12,12 +22,18 @@ System.out.println("Shoe function");
 public void drive(){
 System.out.println("Drive function");
 }
+public void display(){
+System.out.println("Iam the Display function");
+}
+
 }
 public class InterFace{
 public static void main(String args[]){
-A b1=new B();
+B b1=new B();
 b1.show();
 b1.drive();
+b1.display();
+System.out.println("My age is " +C.age);
 }
 }
 //we can not create object of interface
